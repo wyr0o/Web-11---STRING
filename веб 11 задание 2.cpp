b8c88c180edfdfaaf806ff487791c0e2cb86829d
wyr0o <brykin-2006@mail.ru>
2025-04-17 11:04:57 +0300

    Add files via upload

---
+++ веб 11 задание 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	std::string word= { "малина" };
+	std::string guess = 0;	
+	while (word != guess)
+	{
+		std::cout << "Угадайте слово: ";
+		std::cin >> guess;
+		if (word != guess)
+		{
+			std::cout << "Неправильно"<< std::endl;
+		}
+		else 
+		{
+			std::cout << "Правильно! Вы победили! Загаданное слово — "<< word << std::endl;
+		}
+	}
+}
